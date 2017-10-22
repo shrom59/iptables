779b43d6ca8dcd9aa6aec3f454f03ee5222295e8
shrom59 <couillet.romain@yahoo.fr>
2017-10-22 11:11:00 +0200

    Update defaultpolicy.sh

--- commonrules/defaultpolicy.sh
+++ commonrules/defaultpolicy.sh
@@ -14,4 +14,19 @@ _rulesvalue="Drop all forward traffic"
 
 rules "$_dropallforward" "$_rulesvalue"
 
+_dropallinputv6=$(ip6tables -t filter -P INPUT DROP 2>&1 >/dev/null)
+_rulesvalue="Drop all Input IPV6 traffic"
+
+rules "$_dropallinputv6" "$_rulesvalue"
+
+_dropallforwardv6=$(ip6tables -t filter -P FORWARD DROP 2>&1 >/dev/null)
+_rulesvalue="Drop all Forward IPV6 traffic"
+
+rules "$_dropallforwardv6" "$_rulesvalue"
+
+_dropalloutputv6=$(ip6tables -t filter -P OUTPUT DROP 2>&1 >/dev/null)
+_rulesvalue="Drop all Output IPV6 traffic"
+
+rules "$_dropalloutputv6" "$_rulesvalue"
+
 }
