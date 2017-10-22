1030a00e975d53304808203a5d757d73695cad69
shrom59 <couillet.romain@yahoo.fr>
2017-10-22 11:16:00 +0200

    Update dostatus.sh

--- includes/dostatus.sh
+++ includes/dostatus.sh
@@ -2,9 +2,9 @@ dostatus(){
 
 #Display all current rules applied in iptables with the line numbers
 
-echo -e "$_gcolor"" ---------- IPTABLES FOR IPV4 ----------""$_dcolor"
+echo -e "$_gcolor"" ---------- IPTABLES FOR IPV4 ---------- ""$_dcolor"
 iptables -L -n -v --line-numbers
 echo
-echo -e "$_gcolor"" ---------- IPTABLES FOR IPV6 ----------""$_dcolor"
+echo -e "$_gcolor"" ---------- IPTABLES FOR IPV6 ---------- ""$_dcolor"
 ip6tables -L -n -v --line-numbers
 }
