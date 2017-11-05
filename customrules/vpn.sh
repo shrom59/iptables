a0f3a25ba5980fd14be78642749d370a4eceac62
shrom59 <couillet.romain@yahoo.fr>
2017-11-05 13:12:17 +0100

    Update vpn.sh

--- customrules/vpn.sh
+++ customrules/vpn.sh
@@ -1,8 +1,12 @@
+#This rule will allow all UDP connection (inclunding ESP) for strongswan VPN in transport mode
 vpn(){
 
 _ip=1
 
+echo "###### VPN TRANSPORT RULES #####"
+
 fileexist $_ipconf;
+
 
 if [ $exist -eq 1 ];
 then
@@ -14,25 +18,21 @@ then
                 echo -e "$_noip"
         else
                 for i in $_vpnip
-                do
-                      #This rule will allow all UDP connection (inclunding ESP) for strongswan VPN in transport mode
-
-                      echo "###### VPN TRANSPORT RULES #####"
-
+                do                   
                       ##UDP PORT 500
-                      _inputudp500=$(iptables -A INPUT -i eth0 --src $i -p udp --sport 500 --dport 500 -j ACCEPT -m comment --comment "ALLOW IPSEC PORT 500 " 2>&1 >/dev/null)
-                      _rulesvalue500="Allowing UDP 500 port for IPSec"
+                      _inputudp500=$(iptables -A INPUT -i eth0 --src $i -p udp --sport 500 --dport 500 -j ACCEPT -m comment --comment "ALLOW IPSEC FOR $i ON PORT 500" 2>&1 >/dev/null)
+                      _rulesvalue500="Allow IPSEC for $i on 500 port"
 
                       rules "$_inputudp500" "$_rulesvalue500"
 
                       #UDP PORT 4500
-                      _inputudp4500=$(iptables -A INPUT -i eth0 --src $i -p udp --sport 4500 --dport 4500 -j ACCEPT -m comment --comment "ALLOW IPSEC PORT 4500 " 2>&1  >/dev/null)
-                      _rulesvalue4500="Allowing UDP 4500 port for IPSec"
+                      _inputudp4500=$(iptables -A INPUT -i eth0 --src $i -p udp --sport 4500 --dport 4500 -j ACCEPT -m comment --comment "ALLOW IPSEC FOR $i ON PORT 4500" 2>&1  >/dev/null)
+                      _rulesvalue4500="Allow IPSEC for $i on 4500 port"
                       rules "$_inputudp4500" "$_rulesvalue4500"
 
                       ##ESP
-                      _inputesp=$(iptables -A INPUT -i eth0 --src $i -p esp -j ACCEPT -m comment --comment "ALLOW ESP FOR IPSEC")
-                      _rulesvalueesp="Allowing ESP for IPSEC"
+                      _inputesp=$(iptables -A INPUT -i eth0 --src $i -p esp -j ACCEPT -m comment --comment "ALLOW ESP FOR $i")
+                      _rulesvalueesp="Allowing ESP for $i"
                       rules "$_inputesp" "$_rulesvalueesp"                      
                       
                 done
