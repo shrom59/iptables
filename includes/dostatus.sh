34bccd4c6715a8e4c69219f368b8ce473d282374
Romain Couillet <couillet.romain@yahoo.fr>
2019-04-22 11:34:48 +0100

    Fix status

--- includes/dostatus.sh
+++ includes/dostatus.sh
@@ -3,8 +3,11 @@ dostatus(){
 #Display all current rules applied in iptables with the line numbers
 
 echo -e "$_gcolor"" ---------- IPTABLES FOR IPV4 ---------- ""$_dcolor"
-iptables -L -n -v -t nat --line-numbers
+iptables -L -n -v --line-numbers
 echo
 echo -e "$_gcolor"" ---------- IPTABLES FOR IPV6 ---------- ""$_dcolor"
-ip6tables -L -n -v -t nat --line-numbers
+ip6tables -L -n -v --line-numbers
+echo
+echo -e "$_gcolor"" ---------- NAT TABLE ---------- ""$_dcolor"
+iptables -L -n -v -t nat --line-numbers
 }
